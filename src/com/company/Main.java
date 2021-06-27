761ac06720fb9a8819bbfbf9afa3483a46861ddc
Alesya Yurkevich <lesja_1_gv@mail.ru>
2021-06-27 23:59:32 +0300

    summa nechetnyh chisel ot 1 do 99

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 99; i++) {
+            if (i % 2 != 0)
+            sum = sum + i;}
+            System.out.println("Сумма нечетных чисел от 1 до 99 равна " + sum);
+
+
+        }
+    }
+
+
